1a3902cb7e93daa7e3d03efd863af3a0a048e4c8
JatinThareja <anexus7@gmail.com>
2017-02-12 00:38:12 +0530

    Added First Screen

--- app/src/main/java/com/greatapps/jatinthareja/todo/MainActivity.java
+++ app/src/main/java/com/greatapps/jatinthareja/todo/MainActivity.java
@@ -1,13 +1,23 @@
 package com.greatapps.jatinthareja.todo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    ImageView background;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        background= (ImageView) findViewById(R.id.bg_imageview);
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Glide.with(this).load(R.drawable.background).centerCrop().into(background);
+        
     }
 }
